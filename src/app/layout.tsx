cfd0b0368974dff1b4b4ff191bb35c132e023f6f
Khishigbayar <hishigbayr_3@yahoo.com>
2026-05-25 13:03:22 +0800

    fix: check border color, and scale

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
       <body>
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
